9bc72af73b1a4974f86c0932c528f355a6b87023
Chris Koskey <ckoskey@opengov.com>
2016-10-27 22:31:27 -0700

    color!

---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export default [
+  "#96d2e6", "#6d96bf", "#FAF3A5", "#edd287", "#93cebb",
+  "#cc8e8a", "#977fab", "#74acb8", "#e3f2c6", "#e3b7a4",
+  "#B6E3E3", "#548AA1", "#F7D7B2", "#CC7A7A", "#E8C5EB",
+  "#8270A3", "#BAE8C9", "#61877F", "#F2C1D9", "#996A8B"
+];
